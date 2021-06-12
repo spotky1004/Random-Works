98cca947579c4c770f773b5037de1a8a5e00fd41
Spotky1004 <spotky1004@gmail.com>
2021-06-13 03:16:35 +0900

    Update incremental.js

--- Scripts/discord.js/incremental.js
+++ Scripts/discord.js/incremental.js
@@ -1,2 +1,28 @@
-global.Game = class {constructor () {this.point = 0}; increment() {this.point += 1; this.point += Math.floor(this.point*0.1); this.point += Math.floor(this.point**1.01-this.point); return `You have ${this.point} point`}}; global.gameSaveData = new Game(); msg.channel.send("You have 0 point").then(msg => {global.incrementMessage = msg; global.incrementMessage.react(":arrow_up:"); global.updateIncrementMessage(); }); global.updateIncrementMessage = () => {
-    global.incrementMessage.awaitReactions(r => true, {max: 1, time: 60000}).then(_ => {global.incrementMessage.edit(global.gameSaveData.increment()).then(n => global.updateIncrementMessage()).catch(e => global.incrementMessage.channel.send(e))})};
+global.Game = class {
+    constructor () {
+        this.point = 0;
+    };
+    
+    increment() {
+        this.point += 1;
+        this.point += Math.floor(this.point*0.1);
+        this.point += Math.floor(this.point**1.01-this.point);
+        return `You have ${this.point} point`;
+    }
+};
+
+global.gameSaveData = new Game();
+msg.channel.send("You have 0 point").then(
+    msg => {
+        global.incrementMessage = msg;
+        global.incrementMessage.react(":arrow_up:");
+        global.updateIncrementMessage();
+    }
+); global.updateIncrementMessage = () => {
+    global.incrementMessage.awaitReactions(r => true, {max: 1, time: 60000})
+    .then(_ => {
+        global.incrementMessage.edit(global.gameSaveData.increment())
+        .then(n => global.updateIncrementMessage())
+        .catch(e => global.incrementMessage.channel.send(e))
+    })
+};
