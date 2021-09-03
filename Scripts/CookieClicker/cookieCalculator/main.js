46fd4a85519cb364e647aec75dea56b6c7343cfd
Spotky1004 <spotky1004@gmail.com>
2021-09-03 22:11:22 +0900

    cookieCalculator: better style

--- Scripts/CookieClicker/cookieCalculator/main.js
+++ Scripts/CookieClicker/cookieCalculator/main.js
@@ -9,15 +9,15 @@ Game.registerMod("cookie calculator",{
 
 		Game.Notify(`Cookie Calculator!`, `by spotky1004🌲#8812`,[12,10]);
 
-		l("sectionLeft").insertAdjacentHTML('beforeend', `<div id="cookieCalculator" style="position: absolute; top: 2px; left: 2px; z-index: 99; "></div>`);
-		for (let i = 0; i < Config.length + 1; i++) l("cookieCalculator").insertAdjacentHTML('beforeend', `<div class="cookieCalculator-item">Loading...</div>`)
+		l("sectionLeft").insertAdjacentHTML('beforeend', `<div id="cookieCalculator" style="position: relative; top: 2px; left: 2px; z-index: 99; pointer-event: none; text-shadow: 0 0 0.4vh #000;"></div>`);
+		for (let i = 0; i < Config.length + 1; i++) l("cookieCalculator").insertAdjacentHTML('beforeend', `<div class="cookieCalculator-item" style="margin-bottom: 0.05vh;">Loading...</div>`)
 		const items = document.getElementsByClassName("cookieCalculator-item");
 
 		setInterval(function() {
-			items[0].innerText = `${Math.floor(Game.cookies/Game.cookiesPs)}s: ${Beautify(Game.cookies)}`;
+			items[0].innerHTML = `<span style="background-color: #fffa; box-shadow: 0 0 0.2vh #fffa; display: inline-block; width: 5vw; margin-right: 0.4vw; padding: 0.03vh 0; border-radius: 20%; text-align: center; color: #000;">${Math.floor(Game.cookies/Game.cookiesPs)}s</span> ${Beautify(Game.cookies)}`;
 			for (let i = 0; i < Config.length; i++) {
 				const ItemData = Config[i];
-				items[i+1].innerText = `${ItemData[0]}: ${Beautify(Game.cookiesPs*ItemData[1])}`;
+				items[i+1].innerHTML = `<span style="background-color: #fffa; box-shadow: 0 0 0.2vh #fffa; display: inline-block; width: 5vw; margin-right: 0.4vw; padding: 0.03vh 0; border-radius: 20%; text-align: center; color: #000;">${ItemData[0]}</span>${Beautify(Game.cookiesPs*ItemData[1])}`;
 			}
 		}, 1000);
 	},
