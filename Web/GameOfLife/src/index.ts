64f875f59080cf85876df322efe89acb3bbe98ff
Spotky1004 <spotky1004@gmail.com>
2022-05-10 00:29:05 +0900

    GameOfLife) Add randomize feature

--- Web/GameOfLife/src/index.ts
+++ Web/GameOfLife/src/index.ts
@@ -15,14 +15,35 @@ const elements = {
     styles: document.querySelector("input[name='styles']") as HTMLInputElement,
   },
   buttons: {
-    apply: document.getElementById("apply-button") as HTMLButtonElement
+    apply: document.getElementById("apply-button") as HTMLButtonElement,
+    randomize: document.getElementById("randomize-button") as HTMLButtonElement
   }
 };
 
-elements.buttons.apply.addEventListener("click", () => {
+function applyGame() {
   game.applyGame(
     elements.inputs.cells.value,
     elements.inputs.rules.value,
     elements.inputs.styles.value
   );
+}
+elements.buttons.apply.addEventListener("click", applyGame);
+elements.buttons.randomize.addEventListener("click", () => {
+  const x = Math.floor(Math.random()*200)+25;
+  const y = Math.floor(Math.random()*200)+25;
+  const cells = Array(Math.floor(Math.random()*3+2)).fill(null).map(() => Array(Math.floor(Math.random()*3+2)).fill(null).map(() => Math.floor(Math.random()*2)));
+  let cellText = "";
+  for (let dy = 0; dy < cells.length; dy++) {
+    const row = cells[dy];
+    for (let dx = 0; dx < row.length; dx++) {
+      if (row[dx] === 1) cellText += `(${x+dx}, ${y+dy}) `;
+    }
+  }
+  elements.inputs.cells.value = cellText.trim();
+  elements.inputs.rules.value = `B${Array(8).fill("").map((_, i) => Math.random() < 0.3 ? i+1 : "").join("")}/S${Array(9).fill("").map((_, i) => Math.random() < 0.3 ? i : "").join("")}`;
+  const bgColor = "#" + Math.floor(Math.random()*16**6).toString(16).padStart(6, "0");
+  const lifeColor = [Math.floor(Math.random()*255), Math.floor(Math.random()*255), Math.floor(Math.random()*255)];
+  const deadColorTo = lifeColor.map(v => Math.floor(v+Math.random()*30)%255);
+  elements.inputs.styles.value = `{"backgroundColor": "${bgColor}", "lifeColor": [${lifeColor.join(", ")}], "deadColorStages": ${Math.floor(Math.random()*6+1)}, "deadColorFrom": [${lifeColor.join(", ")}], "deadColorTo": [${deadColorTo.join(", ")}]}`
+  applyGame();
 });
